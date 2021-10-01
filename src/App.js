5efaef13debe259e17f9c4fc657c079997c07498
Lars Schlögel <lars.schloegel@gmail.com>
2021-10-01 14:07:35 +0200

    Ergänzung API, Next und Prev Button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Header from "./components/Header";
-import characterResponse from './charactersResponse.json';
 import CharacterCard from './components/CharacterCard';
 import CharacterGallery from "./components/CharacterGallery";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import fetchCharacterPage from "./service/rick-and-morty-api-service";
+import Button from '@mui/material/Button';
 
 
 function App() {
@@ -11,26 +12,56 @@ function App() {
     //const characters = characterResponse.results
     const [characters, setCharacters] = useState([])
     const [search, setSearch] = useState("")
+    const [pageInfo, setPageInfo] = useState({})
 
-    const clickLoadButton = () => {
-        setCharacters(characterResponse.results)
+    useEffect(() => {
+        fetchDataAndSetState()
+    }, [search])
+
+    const loadCharacter = () => {
+        fetchCharacterPage()
+            .then(page => setCharacters(page.results))
     }
-    const clearCharacterButton = () => {
+    const clearCharacter = () => {
         setCharacters([])
     }
-    const searchButton = event => {
+    const searchCharacter = event => {
         const newSearch = event.target.value
         setSearch(newSearch)
     }
+
     const filterCharacters = characters.filter(character =>
         character.name.toLowerCase().includes(search.toLowerCase()))
+
+    function fetchDataAndSetState(url) {
+        fetchCharacterPage(url)
+            .then(page => {
+                setPageInfo(page.info)
+                setCharacters(page.results)
+            })
+            .catch(error => console.error(error.message))
+    }
+
+    const fetchMoreCharacter = () => {
+        fetchDataAndSetState(pageInfo.next)
+    }
+
+    const fetchOtherCharacter = () => {
+        fetchDataAndSetState(pageInfo.prev)
+    }
     return (
         <>
             <div>
                 <Header title="Rick & Morty App"/>
-                <button onClick={clickLoadButton}>Load characters</button>
-                <button onClick={clearCharacterButton}>Clear characters</button>
-                <input type={"text"} onChange={searchButton} value={search}/>
+                <Button sx={{backgroundColor: '#9C2525', marginLeft: 1.5}} variant="contained" onClick={loadCharacter}>Load
+                    characters</Button>
+                <Button sx={{backgroundColor: '#9C2525', marginLeft: 1.5}} variant="contained" onClick={clearCharacter}>Clear
+                    characters</Button>
+                <Button sx={{backgroundColor: '#9C2525', marginLeft: 1.5}} variant="contained" onClick={fetchMoreCharacter}>Next
+                    Page</Button>
+                <Button sx={{backgroundColor: '#9C2525', marginLeft: 1.5}} variant="contained" onClick={fetchOtherCharacter}>Prev
+                    Page</Button>
+                <input type={"text"} onChange={searchCharacter} value={search}/>
                 <CharacterGallery characters={filterCharacters}/>
             </div>
         </>
